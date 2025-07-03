f3606718b35f162ac6e46e998e78378b4406baa5
Mehak021205 <mehak0453.becse24@chitkara.edu.in>
2025-07-03 10:09:37 +0530

    Second code

---
+++ l2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int x,y;
+    cout<<"ENTER A NUMBER"; //cout is used to print output in c++
+    cin>>x; //cin is used to take input in c++
+    cout<<"ENTER 2ND NUMBER";
+    cin>>y;
+    cout<<"product is "<<(x*y);
+    return 0;
+}
